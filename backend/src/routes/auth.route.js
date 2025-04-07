2a8026c4fdb642a574adf3b43d10352829946efb
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-07 15:04:22 +0600

    check auth route done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  login, logout, signup, updateProfile } from "../controllers/auth.controllers.js";
+import {  checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controllers.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -10,6 +10,6 @@ router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
-// router.get("/check", protectRoute, checkAuth);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
